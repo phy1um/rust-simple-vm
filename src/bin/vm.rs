c886cb5c38a7fd3da87e9d412288f7b30eb82081
Tom Marks <tom@tommarks.xyz>
2024-01-04 15:45:19 +1100

    src/bin/vm: manually poke test program into memory

--- src/bin/vm.rs
+++ src/bin/vm.rs
@@ -1,8 +1,26 @@
 
-use simplevm::Machine;
+use simplevm::{Machine, Register};
 
-pub fn main() -> Result<(), &'static str> {
+pub fn main() -> Result<(), String> {
     let mut vm = Machine::new();
-    vm.step()
+    /*
+     * PUSH 10
+     * PUSH 8
+     * ADDSTACK
+     * POPREGISTER A
+     */
+    vm.memory.write(0, 0x1);
+    vm.memory.write(1, 10);
+    vm.memory.write(2, 0x1);
+    vm.memory.write(3, 8);
+    vm.memory.write(4, 0x3);
+    vm.memory.write(6, 0x2);
+    vm.memory.write(7, 0);
+    vm.step()?;
+    vm.step()?;
+    vm.step()?;
+    vm.step()?;
+    println!("A = {}", vm.get_register(Register::A));
+    Ok(())
 }
 
