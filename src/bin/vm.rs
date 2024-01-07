b462c5b34b8591487a5d2f026b951625069d72a7
Tom Marks <tom@tommarks.xyz>
2024-01-07 20:03:55 +1100

    src/bin/vm: load program from file

--- src/bin/vm.rs
+++ src/bin/vm.rs
@@ -1,21 +1,25 @@
+use std::env;
+use std::io::{BufReader, Read};
+use std::path::Path;
+use std::fs::File;
 
 use simplevm::{Machine, Register};
 
 pub fn main() -> Result<(), String> {
+
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        panic!("usage: {} <input>", args[0]);
+    }
+
+    let file = File::open(Path::new(&args[1])).map_err(|x| format!("failed to open: {}", x))?;
+
+    let mut reader = BufReader::new(file);
+    let mut program: Vec<u8> = Vec::new();
+    reader.read_to_end(&mut program).map_err(|x| format!("read: {}", x))?;
+
     let mut vm = Machine::new();
-    /*
-     * PUSH 10
-     * PUSH 8
-     * ADDSTACK
-     * POPREGISTER A
-     */
-    vm.memory.write(0, 0x1);
-    vm.memory.write(1, 10);
-    vm.memory.write(2, 0x1);
-    vm.memory.write(3, 8);
-    vm.memory.write(4, 0x3);
-    vm.memory.write(6, 0x2);
-    vm.memory.write(7, 0);
+    vm.memory.load_from_vec(&program, 0);
     vm.step()?;
     vm.step()?;
     vm.step()?;
