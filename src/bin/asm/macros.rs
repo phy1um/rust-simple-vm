095e7c5fec6d62703ffcea383987c45363c21711
Tom Marks <tom@tommarks.xyz>
2024-02-27 11:25:54 +1100

    whatever i was doing last stream

--- src/bin/asm/macros.rs
+++ src/bin/asm/macros.rs
@@ -19,12 +19,34 @@ pub fn include(_pp: &mut PreProcessor, input: Vec<&str>) -> Result<Vec<String>, 
         return Err(format!("requires exactly 1 argument, got {}", input.len()));
     }
     let file_name = input.get(0).unwrap();
-    let file = File::open(Path::new(&file_name)).map_err(
-        |x| format!("failed to open \"{}\": {}", file_name, x))?;
+    let file = File::open(Path::new(&file_name))
+        .map_err(|x| format!("failed to open \"{}\": {}", file_name, x))?;
     let mut out: Vec<String> = Vec::new();
     for line in BufReader::new(file).lines() {
         let line_inner = line.map_err(|x| format!("{}", x))?;
         out.push(line_inner);
     }
     Ok(out)
+}
+
+pub fn defmacro(pp: &mut PreProcessor, input: Vec<&str>) -> Result<Vec<String>,String> {
+    if input.is_empty() {
+        return Err(format!("no arguments"));
+    }
+    let macro_name = input.get(0).unwrap();
+    let mut lines: Vec<String> = Vec::new();
+    let mut current_line: Vec<String> = Vec::new();
+    for &token in &input[1..] {
+        if token != "/" {
+            current_line.push(token.to_string());  
+        } else {
+            lines.push(current_line.join(" "));
+            current_line.clear();
+        }
+    }
+    if !current_line.is_empty() {
+        lines.push(current_line.join(" "));
+    }
+    pp.define_subst_macro(macro_name, lines);
+    Ok(Vec::new())
 }
--- src/bin/asm/pp.rs
+++ src/bin/asm/pp.rs
@@ -1,4 +1,3 @@
-
 use std::collections::HashMap;
 use std::fmt;
 
@@ -19,10 +18,14 @@ impl fmt::Display for Error {
 }
 
 type MacroFunc = fn(&mut PreProcessor, input: Vec<&str>) -> Result<Vec<String>, String>;
+pub enum Macro {
+    Func(MacroFunc),
+    Subst(Vec<String>),
+}
 
 pub struct PreProcessor {
     variables: HashMap<String, String>,
-    macros: HashMap<String, MacroFunc>,
+    macros: HashMap<String, Macro>,
     instruction_count: u32,
 }
 
@@ -45,17 +48,47 @@ impl PreProcessor {
                 Some(';') => return Ok(input.to_string()),
                 Some('.') => {
                     let name = &head[1..];
-                    let func = self.get_macro(name).ok_or(
-                            Error::UnknownToken(head[1..].to_string()))?;
-                    let res = func(self, parts[1..].to_vec()).map_err(|x| Error::MacroEval(name.to_string(), x))?;
-                    let resolved: Result<Vec<String>, Error> = res.into_iter().map(|line| {
-                        self.resolve(&line)
-                    }).collect();
+                    let func = self
+                        .get_macro(name)
+                        .ok_or(Error::UnknownToken(head[1..].to_string()))?;
+                    let res = match func {
+                        Macro::Func(f) => {
+                            f(self, parts[1..].to_vec())
+                                .map_err(|x| Error::MacroEval(name.to_string(), x))?
+                        },
+                        Macro::Subst(lines) => {
+                            lines.into_iter().map(|line| {
+                                let mp: Result<Vec<String>,String> = line.split(" ").map(|p| {
+                                    match p.chars().nth(0) {
+                                        Some('!') => {
+                                            match u32::from_str_radix(&p[1..], 10) {
+                                                Ok(n) => {
+                                                    parts.get((n+1) as usize).ok_or("failed to get".to_string()).map(|x| x.to_string())
+                                                },
+                                                Err(e) => {
+                                                    Ok(p.to_string())
+                                                    // Err(format!("parse {}: {}", p, e))
+                                                }
+                                            }
+                                        },
+                                        _ => Ok(p.to_string()),
+                                    }
+                                }).collect();
+                                // TODO: handle error here
+                                match mp {
+                                    Ok(s) => s.join(" "),
+                                    Err(e) => format!("err: {}", e),
+                                }
+                            }).collect()
+                        }
+                    };
+                    let resolved: Result<Vec<String>, Error> =
+                        res.into_iter().map(|line| self.resolve(&line)).collect();
                     return Ok(resolved?.join("\n"));
                 }
                 Some(':') => {
                     let name = &head[1..];
-                    let offset = format!("{}", self.instruction_count*2);
+                    let offset = format!("{}", self.instruction_count * 2);
                     self.define_variable(name, &offset);
                     return Ok(String::new());
                 }
@@ -63,14 +96,11 @@ impl PreProcessor {
             }
         }
         // resolved :: Vec<Result<String, Error>>
-        let resolved = parts.into_iter().map(|p| {
-            match p.chars().nth(0) {
-                Some('!') => {
-                    self.get_variable(&p[1..]).ok_or(
-                        Error::UnknownToken(p[1..].to_string()))
-                }
-                _ => Ok(p.to_string())
-            }
+        let resolved = parts.into_iter().map(|p| match p.chars().nth(0) {
+            Some('!') => self
+                .get_variable(&p[1..])
+                .ok_or(Error::UnknownToken(p[1..].to_string())),
+            _ => Ok(p.to_string()),
         });
         let st: Result<Vec<String>, Error> = resolved.into_iter().collect();
         self.instruction_count += 1;
@@ -85,11 +115,15 @@ impl PreProcessor {
         self.variables.insert(name.to_string(), value.to_string());
     }
 
-    fn get_macro(&mut self, name: &str) -> Option<&MacroFunc> {
+    fn get_macro(&mut self, name: &str) -> Option<&Macro> {
         self.macros.get(name)
     }
 
     pub fn define_macro(&mut self, name: &str, value: MacroFunc) {
-        self.macros.insert(name.to_string(), value);
+        self.macros.insert(name.to_string(), Macro::Func(value));
+    }
+
+    pub fn define_subst_macro(&mut self, name: &str, value: Vec<String>) {
+        self.macros.insert(name.to_string(), Macro::Subst(value));
     }
 }
