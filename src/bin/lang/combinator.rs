d6615aa156296dca9005a5841068acd52aafb2a9
Tom Marks <tom@tommarks.xyz>
2024-06-06 19:21:45 +0930

    src/bin/lang: add any and require combinators

--- src/bin/lang/combinator.rs
+++ src/bin/lang/combinator.rs
@@ -31,6 +31,33 @@ impl<S, T, E, F: Parser<S, T, E>> Parser<S, Vec<T>, E> for Sequence<S, T, E, F> 
     }
 }
 
+#[derive(Debug, Default)]
+pub struct Any<S, T, E, F>
+where F: Parser<S, T, E>
+{
+    _t: PhantomData<(S, T, E)>,
+    items: Vec<F>,
+}
+
+impl <S, T, E, F> Any<S, T, E, F> 
+where F: Parser<S, T, E>
+{
+    pub fn new(items: Vec<F>) -> Self {
+        Self {items, _t: PhantomData::default() }
+    }
+}
+
+impl<S: Clone, T, E, F: Parser<S, T, E>> Parser<S, Option<T>, E> for Any<S, T, E, F> {
+    fn run(&self, s: S) -> Result<(S, Option<T>), E> {
+        for item in &self.items {
+            if let Ok((sn, res)) = item.run(s.clone()) {
+                return Ok((sn, Some(res)))
+            };
+        };
+        Ok((s, None))
+    }
+}
+
 pub fn map<S, T, E, U, F, G>(f: F, g: G) -> impl Fn(S) -> Result<(S, U), E> 
 where F: Parser<S,T,E>,
       G: Fn(T) -> U,
@@ -38,6 +65,19 @@ where F: Parser<S,T,E>,
     move |input| {
         let (s, res) = f.run(input)?; 
         Ok((s, g(res)))
+    }
+}
+
+pub fn require<S, T, E: Default, F>(f: F) -> impl Fn(S) -> Result<(S, T), E>
+where F: Parser<S, Option<T>, E>
+{
+    move |input| {
+        let (s, res) = f.run(input)?;
+        if let Some(t) = res {
+            Ok((s, t))
+        } else {
+            Err(E::default())
+        }
     }
 }
 
@@ -51,6 +91,12 @@ pub fn repeat1<S: Clone, T, E: Clone + Default, F>(f: F) -> impl Fn(S) -> Result
 where F: Parser<S, T, E>
 {
     repeat(1, E::default(), f)
+}
+
+pub fn discard<S, T, E, F>(f: F) -> impl Fn(S) -> Result<(S, ()), E> 
+where F: Parser<S, T, E>
+{
+    map(f, |_| ())
 }
 
 fn repeat<S: Clone, T, E: Clone, F>(min_matches: usize, err: E, f: F) -> impl Fn(S) -> Result<(S, Vec<T>), E> 
@@ -74,3 +120,52 @@ where F: Parser<S, T, E>
     }
 }
 
+fn wrapped<A, B, C, S, E, F, G, H>(f: F, g: G, h: H) -> impl Fn(S) -> Result<(S, B), E> 
+where F: Parser<S, A, E>,
+      G: Parser<S, B, E>,
+      H: Parser<S, C, E>,
+{
+    move |input| {
+        let (input0, _) = f.run(input)?;
+        let (input1, res) = g.run(input0)?;
+        let (input2, _) = h.run(input1)?;
+        Ok((input2, res))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::character::*;
+
+    #[test]
+    fn test_wrapped() {
+        {
+            let (s, n) = wrapped(token("{"), alpha, token("}"))("{a}").unwrap();
+            assert_eq!('a', n); 
+            assert_eq!("", s); 
+        }
+        {
+            let (s, n) = wrapped(token("<<"), map(repeat1(alpha), |x| x.iter().collect::<String>()), token("}}"))("<<foobar}}").unwrap();
+            assert_eq!("foobar", n); 
+            assert_eq!("", s); 
+        }
+        {
+            let (s, n) = wrapped(
+                token("\""), 
+                map(repeat1(not_char("\"")), |x| x.iter().collect::<String>()),
+                token("\""),
+            )("\"
+Hello world this is some text. \"").unwrap();
+            assert_eq!("\nHello world this is some text. ", n);
+            assert_eq!("", s);
+        }
+    }
+
+    #[test]
+    fn test_any() {
+        let (s, n) = require(Any::new(vec![alpha, numeric]))("1").unwrap();
+        assert_eq!("", s);
+        assert_eq!('1', n);
+    }
+}
