fdb36d1c965fda22cc584053aa159435663d48d6
Tom Marks <tom@tommarks.xyz>
2024-10-31 13:08:55 +1100

    lang/src/compile/codegen: optimize noop pass

--- lang/src/compile/codegen/optimize.rs
+++ lang/src/compile/codegen/optimize.rs
@@ -1,8 +1,12 @@
 use crate::compile::block::Block;
 use log::trace;
-use simplevm::{resolve::UnresolvedInstruction, Instruction};
+use simplevm::{resolve::UnresolvedInstruction, Instruction, Register};
 
 pub(crate) fn optimize_function(block: &mut Block) {
+    remove_noops(block);
+}
+
+pub(crate) fn remove_noops(block: &mut Block) {
     let mut new_instructions = Vec::new();
     for instruction in &block.instructions {
         if let Some(i) = match instruction {
@@ -15,6 +19,41 @@ pub(crate) fn optimize_function(block: &mut Block) {
             }
             UnresolvedInstruction::Instruction(Instruction::AddImmSigned(_, lit)) => {
                 if lit.value == 0 {
+                    None
+                } else {
+                    Some(instruction)
+                }
+            }
+            UnresolvedInstruction::Instruction(Instruction::Add(tgt, a, b)) => {
+                if (tgt == a && *b == Register::Zero) || (tgt == b && *a == Register::Zero) {
+                    None
+                } else {
+                    Some(instruction)
+                }
+            }
+            UnresolvedInstruction::Instruction(Instruction::Sub(tgt, a, b)) => {
+                if (tgt == a && *b == Register::Zero) || (tgt == b && *a == Register::Zero) {
+                    None
+                } else {
+                    Some(instruction)
+                }
+            }
+            UnresolvedInstruction::Instruction(Instruction::ShiftLeft(tgt, a, n)) => {
+                if (tgt == a && n.value == 0) {
+                    None
+                } else {
+                    Some(instruction)
+                }
+            }
+            UnresolvedInstruction::Instruction(Instruction::ShiftRightLogical(tgt, a, n)) => {
+                if (tgt == a && n.value == 0) {
+                    None
+                } else {
+                    Some(instruction)
+                }
+            }
+            UnresolvedInstruction::Instruction(Instruction::ShiftRightArithmetic(tgt, a, n)) => {
+                if (tgt == a && n.value == 0) {
                     None
                 } else {
                     Some(instruction)
